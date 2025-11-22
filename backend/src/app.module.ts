0958a60ae031739210a894a37f5ee5f64cbea748
XiaoLong Ji <jixiaolong2003@gmail.com>
2025-11-22 14:39:56 +0100

    Add Mobility module, controller, and service with tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LifestyleModule } from './lifestyle/lifestyle.module';
 import { ServicesModule } from './services/services.module';
+import { MobilityController } from './mobility/mobility.controller';
+import { MobilityModule } from './mobility/mobility.module';
 
 @Module({
-  imports: [LifestyleModule, ServicesModule],
-  controllers: [AppController],
+  imports: [LifestyleModule, ServicesModule, MobilityModule],
+  controllers: [AppController, MobilityController],
   providers: [AppService],
 })
 export class AppModule {}
